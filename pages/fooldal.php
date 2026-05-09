edeb5601d10cbe65b5607df43d090583bab08a57
kovacsbalazs0221 <kovacsbalazs0221@gmail.com>
2026-05-09 17:56:38 +0200

    Add files via upload

---
+++ pages/fooldal.php
@@ -0,0 +1,23 @@
+<div class="fooldal-tartalom">
+    <h2>Üdvözöljük a Balatoni hajók portálon!</h2>
+    <p>Ismerje meg a balatoni hajózás múltját és jelenét a XIX. század közepétől napjainkig.</p>
+
+    <div class="videok-resz">
+        <h3>Hajózás a Balatonon</h3>
+        <div class="video-grid" style="display: flex; gap: 20px; flex-wrap: wrap; justify-content: center; margin-bottom: 30px;">
+            <video width="400" height="250" controls>
+                <source src="./kepek/rovid_hajo.mp4" type="video/mp4">
+
+                A böngészője nem támogatja a videó lejátszását.
+            </video>
+            
+            <iframe width="400" height="250" src="https://www.youtube.com/embed/rt5ITijoz3Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+    </div>
+
+    <div class="terkep-resz">
+        <h3>Itt talál meg minket (Balatoni Hajózási Zrt. - Siófok)</h3>
+       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2728.463690623253!2d18.046907476839!3d46.90352293700021!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476995646f903825%3A0x6338562d192a5494!2zQmFsYXRvbmkgSGFas8standardIFrYnQuIC0gS2lrw7Z0xZE!5e0!3m2!1shu!2shu!4v1715257000000!5m2!1shu!2shu" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+</div>
+
